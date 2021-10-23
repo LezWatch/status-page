57e90464103ba87f892d34b5cc0bac7373572efe
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2021-10-23 16:32:07 -0700

    Improved information and time stuff

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title>LezWatch Status Pages</title>
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
 	</head>
 
 	<body class="bg-light">
@@ -18,9 +18,8 @@
 			<main>
 				<div class="py-5">
 					<h2>LezWatch Status</h2>
-					<p class="lead">We continuously monitor the status of LezWatch.TV and all its related services. If there are any service interruptions or maintenances, information will be available here. </p>
+					<p class="lead">We continuously monitor the status of LezWatch.TV and all its related services. If there are any interruptions, outages, or maintenances, information will be available here.</p>
 				</div>
-
 
 				<div class="row row-cols-1 row-cols-md-2 g-4">
 				<?php
@@ -30,7 +29,7 @@
 					<div class="col">
 						<div class="card">
 							<div class="card-body">
-								<p>Monitors are currently paused <i class="bi bi-shield-slash"></i></p>
+								<p>All monitors are currently paused <i class="bi bi-shield-slash"></i></p>
 								<p>This may be for website updates/maintenance. Please check again later for an updated status report.</p>
 							</div>
 						</div>
@@ -40,15 +39,18 @@
 					foreach ( $websites as $website ) {
 						?>
 						<div class="col">
-							<div class="card <?= $website['border']; ?>">
-								<h5 class="card-header"><a href="<?= $website['website_url']; ?>"><?= $website['website_name']; ?></a></h5>
+							<div class="card border-<?php echo $website['class']; ?>">
+								<h5 class="card-header"><?php echo $website['monitor_status']; ?></h5>
 								<div class="card-body">
-									<h5 class="card-title"><?= $website['monitor_status']; ?></h5>
-									<p class="card-text"><?= $website['monitor_info']; ?></p>
-									<p class="card-text">Overall uptime (last 30 days): <?= $website['monitor_uptime']; ?>%</p>
-									<a href="<?= $website['website_url']; ?>" class="btn btn-primary">Visit <?= $website['website_name']; ?></a>
+
+									<p class="card-text">Overall uptime (last 30 days): <?php echo $website['monitor_uptime']; ?>%</p>
+
+									<p class="card-text"><?php echo $website['monitor_info']; ?></p>
+
+									<a href="<?php echo $website['monitor_link']; ?>" class="btn btn-secondary" target="_blank">Service Details</a>
+									<a href="<?php echo $website['website_url']; ?>" class="btn btn-primary" target="_blank">Visit <?php echo $website['website_name']; ?></a>
 								</div>
-								<div class="card-footer"><small class="text-muted">Monitoring started <?= $website['monitor_started']; ?></small></div>
+								<div class="card-footer bg-<?php echo $website['class']; ?>"><small class="text-white-50">Monitoring started <?php echo $website['monitor_started']; ?></small></div>
 							</div>
 						</div>
 						<?php
@@ -60,8 +62,11 @@
 			</main>
 
 			<footer class="my-5 pt-5 text-muted text-center text-small">
-				<p class="mb-1">&copy; <?php echo date( 'Y' ); ?> LezPress</p>
-				<p class="mb-1">Powered by <a href="https://stats.uptimerobot.com/926OOTk28x">Uptime Robot</a></p>
+				<p class="mb-1">&copy; <?php echo date( 'Y' ); ?> LezPress | Powered by <a href="<?php echo $uptime_url; ?>">Uptime Robot</a></p>
+
+				<p class="mb-1"><a href="https://github.com/LezWatch/status-page/" target="_blank"><i class="bi bi-github"></a></i> <a href="https://twitter.com/lezwatchtv/" target="_blank"><i class="bi bi-twitter"></i></a></p>
+
+				<p class="mb-1">Licened <a href="/LICENSE">MIT</a></p>
 			</footer>
 
 		</div>
--- monitoring.php
+++ monitoring.php
@@ -1,12 +1,24 @@
-<?php // Get UpTimeRobot stats via cURL:
+<?php
 
-$filename  = 'status.txt';
-$errorfile = 'error.log';
+// Get UpTimeRobot stats via cURL:
+
+require 'time.php';
+
+// Error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Defaults
+$uptime_url = 'https://stats.uptimerobot.com/926OOTk28x/';
+$filename   = 'status.txt';
+$errorfile  = 'error.log';
 
 if ( file_exists( './' . $filename ) ) {
 	// file older than 2 minutes OR is empty
 	if ( ( filesize( $filename ) < 1 ) || ( time()-filemtime( $filename ) > 250 ) ) {
 
+		// Rebuild the file
 		$f = @fopen( $filename, 'r+' );
 		if ( $f !== false ) {
 			ftruncate( $f, 0 );
@@ -21,8 +33,10 @@ if ( file_exists( './' . $filename ) ) {
 			'lezpress'  => 'm782395488-dc56250afd0cf12ca68857c9',
 		);
 
-		$all_responses = array(); // Empty for now.
+		// Prepare array
+		$all_responses = array();
 
+		// Loop through the keys to get the data
 		foreach ( $api_keys as $site => $api_key ) {
 			// Build request:
 			$request = 'api_key=' . $api_key . '&format=json&logs=1&log_types=1&logs_limit=1&all_time_uptime_ratio=1';
@@ -43,13 +57,13 @@ if ( file_exists( './' . $filename ) ) {
 					'content-type: application/x-www-form-urlencoded'
 				),
 			));
-			$response     = curl_exec( $curl );
-			$err_response = curl_error( $curl );
+			$response = curl_exec( $curl );
+			$error    = curl_error( $curl );
 			curl_close( $curl );
 
 			// Log Errors
-			if ( !empty( $err_response ) ) {
-				file_put_contents( $errorfile, json_encode( $err_response ) );
+			if ( !empty( $error ) ) {
+				file_put_contents( $errorfile, json_encode( $error ) );
 			}
 
 			// Decode JSON response and get only the data needed:
@@ -57,18 +71,19 @@ if ( file_exists( './' . $filename ) ) {
 
 			// If there's a monitor value, we save it.
 			if ( ! empty ( $response['monitors'][0] ) ) {
-				$response = $response['monitors'][0];
+				$response               = $response['monitors'][0];
 				$all_responses[ $site ] = $response;
 			} else {
 				file_put_contents( $errorfile, json_encode( $response ) );
 			}
 		}
 
-		// save in the file
+		// save the data in the file
 		file_put_contents( $filename, json_encode( $all_responses ) );
 	}
 }
 
+// Since we have responses for sure, grab the file:
 $all_responses = json_decode( file_get_contents( $filename ), true );
 $websites      = array();
 
@@ -80,71 +95,70 @@ if ( ! empty( $all_responses ) && is_array( $all_responses ) ) {
 			$website_name = $response['friendly_name'];
 			$website_url  = $response['url'];
 
+			// Monitor Details
+			$monitor_link = $uptime_url . $response['id'];
+
 			// Date monitor was created:
 			$monitor_started = $response['create_datetime'];
-			$monitor_started = date('jS F Y', $monitor_started);
+			$monitor_started = date( 'jS F Y', $monitor_started );
 
 			// Overall uptime percentage:
 			$monitor_uptime = $response['all_time_uptime_ratio'];
-			$monitor_uptime = number_format($monitor_uptime, 2);
-
-			// Overall downtime percentage:
-			$monitor_downtime = 100 - $monitor_uptime;
-			$monitor_downtime = number_format($monitor_downtime, 2);
+			$monitor_uptime = number_format( $monitor_uptime, 2 );
 
 			// Current website status:
 			$monitor_status = $response['status'];
 
 			// Change content to be displayed based on current website status:
-			if ($monitor_status == 0) { // Monitor is paused:
+			if ( $monitor_status == 0 ) { // Monitor is paused:
 
-				$monitor_status = 'Monitor is paused <span class="link-info"><i class="bi bi-shield-slash"></i></span>';
+				$class        = 'info';
+				$monitor_status = $website_name . ' monitoring is currently <strong class="text-' . $class . '">paused</strong> <span class="link-info"><i class="bi bi-shield-slash"></i></span>';
 				$monitor_info   = 'This may be for website updates/maintenance. Please check again later for an updated status report';
-				$border         = 'border-info';
 
-			} elseif ($monitor_status == 2) { // Website is up:
+			} elseif ( $monitor_status == 2 ) { // Website is up:
 
-				$monitor_status = 'Website is currently UP <span class="link-success"><i class="bi bi-shield-check"></i></span>';
-				$border         = 'border-success';
+				$class        = 'success';
+				$monitor_status = $website_name . ' is currently <strong class="text-' . $class . '">operational</strong> <span class="link-success"><i class="bi bi-shield-check"></i></span>';
 
 				// Check if there has been any recorded downtime:
-				if (empty($response->logs)) { // Downtime recorded:
-					$monitor_info = 'There has been no recorded downtime in the last 7 days.';
-				} else { // No downtime recorded:
+				if ( empty( $response['logs'] ) ) {
+					// No downtime recorded:
+					$monitor_info = 'There has been no recorded downtime for ' . $website_name . '.';
+				} else {
+					// Date and time since last downtime:
+					$last_downtime = date('jS F Y', $response['logs'][0]['datetime'] );
+					$time_since = get_time_ago( $response['logs'][0]['datetime'] );
 
-					// Get date of last downtime:
-					$monitor_last_downtime = $response['logs'][0]['datetime'];
-					$monitor_last_downtime = date('jS F Y', $monitor_last_downtime);
+					// Duration of last downtime:
+					$last_duration = time() - $response['logs'][0]['duration'];
+					$duration = get_time_ago( $last_duration );
 
-					// Get time since last downtime in hours:
-					$time_downtime = strtotime($monitor_last_downtime);
-					$time_current = time();
-					$time_since_downtime = $time_current - $time_downtime;
-					$time_since_downtime = floor($time_since_downtime / 3600);
-
-					$monitor_info = 'It has been ' . $time_since_downtime . ' hours (' . $monitor_last_downtime . ') since any downtime.';
-
+					// Output:
+					$monitor_info = 'It has been ' . $time_since . ' since the last recorded downtime (' . $last_downtime . ') which lasted ' . $duration . '.';
 				}
+			} elseif ( $monitor_status == 9 ) {
+				// Website is down:
 
-			} elseif ($monitor_status == 9) { // Website is down:
-
-				$monitor_status = 'Website is currently DOWN <span class="link-danger"><i class="bi bi-shield-exclamation"></i></span>';
-				$border         = 'border-info';
+				$class        = 'danger';
+				$monitor_status = $website_name . ' is currently <strong class="text-' . $class . '">down</strong> <span class="link-danger"><i class="bi bi-shield-exclamation"></i></span>';
 
 				// Get length of current downtime in hours:
-				$monitor_downtime_seconds = $response['logs'][0]['duration']; // Seconds
-				$monitor_downtime_hours = floor($monitor_downtime_seconds / 3600); // Hours
-				$monitor_info = 'The website is currently down. It has been down for' . $monitor_downtime_hours . ' hours.';
+				$last_duration = time() - $response['logs'][0]['duration'];
+				$duration = get_time_ago( $last_duration );
+				$monitor_info     = $website_name . ' has been down for' . $duration . '.';
 			}
 
+			// All the collected information.
 			$websites[] = array(
 				'website_name'    => $website_name,
 				'website_url'     => $website_url,
+				'monitor_link'    => $monitor_link,
 				'monitor_started' => $monitor_started,
 				'monitor_uptime'  => $monitor_uptime,
 				'monitor_status'  => $monitor_status,
 				'monitor_info'    => $monitor_info,
-				'border'          => $border,
+				'class'           => $class,
 			);
 		}
 	}
